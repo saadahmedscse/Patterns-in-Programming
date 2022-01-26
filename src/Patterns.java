c5f189842c36ed701000dd3468d9b0ce0b927882
Saad Ahmed <saadahmedscse@gmail.com>
2022-01-26 21:22:27 +0600

    Added new pattern

--- src/Patterns.java
+++ src/Patterns.java
@@ -131,4 +131,17 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void Rhombus(int row){
+        for (int i=1; i<=row; i++){
+            for (int j=row; j>i; j--){
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=row; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
