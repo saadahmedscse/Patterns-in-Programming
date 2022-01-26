524627c4f9d0174b414c0606d1cf6e53c158c84d
Saad Ahmed <saadahmedscse@gmail.com>
2022-01-26 21:25:23 +0600

    Added new pattern

--- src/Patterns.java
+++ src/Patterns.java
@@ -144,4 +144,17 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void MirrorRhombus(int row){
+        for (int i=1; i<=row; i++){
+            for (int j=1; j<i; j++){
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=row; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
